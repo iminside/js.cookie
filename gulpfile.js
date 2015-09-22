5935d9e830f56645fd2cdf94727291624d43284a
4urbanoff <4urbanoff@gmail.com>
2015-09-22 11:11:33 +0300

    upd gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
-var gulp   = require( "gulp" ),
-    babel  = require( "gulp-babel" );
+var gulp  = require( "gulp" ),
+    babel = require( "gulp-babel" ),
+    src   = "./src/**/*.js",
+    dst   = "./dst";
 
 
 gulp.task( "build", function(){
-  return gulp.src( "./src/**/*.js" )
+  return gulp.src( src )
     .pipe( babel() )
-    .pipe( gulp.dest( "./dst" ) );
+    .pipe( gulp.dest( dst ) );
 });
+
+gulp.task( "watch", function(){
+  return gulp.watch( src, [ "build" ] )
+});
+
+gulp.task( "default", [ "build", "watch" ] );
